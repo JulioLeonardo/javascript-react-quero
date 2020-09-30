fd4794e0dab46a40df063c3a5f03c84e022728ad
Julio Leonardo <juleolica@gmail.com>
2020-09-30 01:21:07 -0300

    add btn styling

--- src/components/ScholarshipAddBtn/index.js
+++ src/components/ScholarshipAddBtn/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import './styles.css';
+
 function ScholarshipAddBtn() {
   return (
     <div className="add-scholarship">
-      <div className="plus-icon">
-        <i className="fas fa-plus" />
-      </div>
+      <button className="add-scholarship-btn" onClick>
+        <div className="plus-icon">
+          <i className="fas fa-plus" />
+        </div>
+      </button>
       <h3 className="scholarship-title">Adicionar bolsa</h3>
       <p className="add-scholarship-description">
         Clique para adicionar bolsas de
